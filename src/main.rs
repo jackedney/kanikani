a8c7d9bab152591f51cdbde1b4bbff0657d59ded
jack.edney <jack.edney@outlook.com>
2024-04-25 23:22:59 +0100

    Starting on CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+use std::fs;
+
+static LOGO_ART_PATH: &str = "src/art/kanilogo.txt";
+static NAME_ART_PATH: &str = "src/art/kaniname.txt";
+
 fn main() {
-    println!("こんにちは, 世界!");
+    let logo_art = fs::read_to_string(LOGO_ART_PATH).expect("Failed to read wanilogo.txt file");
+    let name_art = fs::read_to_string(NAME_ART_PATH).expect("Failed to read waniname.txt file");
+
+    // Print the ASCII art sequentially
+    println!("{}", logo_art);
+    println!("{}", name_art);
+
+    // print line across the terminal
+    println!("{}", "-".repeat(80));
+    println!("ようこそ！ Welcome to kanikani - the CLI tool for doing your WaniKani reviews!");
 }
